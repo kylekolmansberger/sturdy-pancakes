db69da218bc08fecbb48b494d3283d3781ad4421
fyl5079 <fyl5079@110-WL38.up.ist.local>
2016-10-31 11:46:27 -0400

    new stufff

--- SturdyPancakes/src/WelcomeScreen.java
+++ SturdyPancakes/src/WelcomeScreen.java
@@ -13,8 +13,10 @@ import javax.swing.*;
  *
  * @author fyl5079
  */
-public class WelcomeScreen extends JPanel{
+public class WelcomeScreen extends JPanel implements ActionListener{
     private JButton clickToContinue;
+    private Timer tm;
+    private int x,velx;
     public WelcomeScreen(){
         super();
         setVisible(true);
@@ -25,6 +27,33 @@ public class WelcomeScreen extends JPanel{
         clickToContinue.setContentAreaFilled(false);
         add(clickToContinue, BorderLayout.CENTER);
         
+        tm = new Timer(5, this);
+        x = 0;
+        velx = 2;
+    
+    
+        
+    }
+    
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.setColor(new Color(156, 93, 82));
+        g.drawOval(x,x,50,30);
+        g.fillOval(x, x, 50, 30);
+        
+        tm.start();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if (x < 0 || x > 700)
+            velx = -velx;
+        
+        x = x + velx;
+        repaint();
     }
     public JButton getClickToContinue() {
         return clickToContinue;
