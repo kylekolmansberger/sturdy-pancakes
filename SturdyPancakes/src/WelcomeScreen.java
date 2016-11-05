7c1388e4c2409ea462e76a0b5c266122b756744d
Frank <Macks@66.71.78.148>
2016-11-04 22:43:36 -0400

    use your first name and last name as username, and password:password ex: username:Frank Liang password:password

--- SturdyPancakes/src/WelcomeScreen.java
+++ SturdyPancakes/src/WelcomeScreen.java
@@ -17,7 +17,9 @@ public class WelcomeScreen extends JPanel implements ActionListener{
     private JButton clickToContinue;
     private Timer tm;
     private int x,y,velx,vely;
-    private Image pancake;
+    private ImageIcon pancakeImage,pancakeImage2;
+    private JLabel pancake,pancake2;
+    private Rectangle bounds;
     public WelcomeScreen(){
         super();
         setVisible(true);
@@ -28,13 +30,29 @@ public class WelcomeScreen extends JPanel implements ActionListener{
         clickToContinue.setContentAreaFilled(false);
         clickToContinue.setBorderPainted(false);
         clickToContinue.setFocusable(false);
+        
+        bounds = new Rectangle(0,0,236,213);
+        
+        pancakeImage = new ImageIcon("images/pancake.jpg");
+        pancake = new JLabel(pancakeImage);
+        pancake.setBounds(bounds);
+        
+        pancakeImage2 = new ImageIcon("images/pancake.png");
+        pancake2 = new JLabel(pancakeImage2);
+        pancake2.setBounds(bounds);
+        
+        
+        add(pancake);
+        add(pancake2);
         add(clickToContinue, BorderLayout.CENTER);
+        
         
         tm = new Timer(5, this);
         x = 0;
         y = 0;
         velx = 3;
         vely = 2;
+        tm.start();
     }
     
     @Override
@@ -45,24 +63,25 @@ public class WelcomeScreen extends JPanel implements ActionListener{
         g.drawOval(x,y,50,30);
         g.fillOval(x, y, 50, 30);
         
-        tm.start();
-        
     }
     
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        if (x < 0 || x > 1280){
+        if(x < 0 || x > 1180){
             velx = -velx;
         }
-        if (y < 0 || y > 720){
+        if(y < 0 || y > 620){
             vely = -vely;
         }
             x = x + velx;
             y = y + vely;
+            bounds = new Rectangle(x,y,200,200);
+            pancake.setBounds(bounds);
             
+            bounds = new Rectangle(x+42,y+42,200,200);
+            pancake2.setBounds(bounds);
             repaint();
-        
     }
     public JButton getClickToContinue() {
         return clickToContinue;
