5ed2731fa00d87b118898838704e915250610b39
fyl5079 <fyl5079@IST113-61.win.psu.edu>
2016-11-09 18:53:40 -0500

    pls work plz

--- SturdyPancakes/src/GeneratePassword.java
+++ SturdyPancakes/src/GeneratePassword.java
@@ -22,22 +22,20 @@ public class GeneratePassword extends JPanel{
         setBackground(Color.white);
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+        
+        backButton = new JButton("< Main Menu");
+        backButton.setOpaque(false);
+        backButton.setContentAreaFilled(false);
+        c.anchor = GridBagConstraints.NORTHWEST;
+        add(backButton,c);
+        
         c.gridwidth = 1;
         c.gridheight = 1;
         c.weightx = 1;
         c.weighty = 1;
         c.ipady =60;
         
-        backButton = new JButton("< Main Menu");
         
-        backButton.setOpaque(false);
-        backButton.setContentAreaFilled(false);
-        c.anchor = GridBagConstraints.NORTHWEST;
-        c.gridx = 0;
-        c.gridy = 0;
-        add(backButton,c);
-        
-        c.anchor = GridBagConstraints.WEST;
         
         passwordOptions = new JLabel("Choose Password Parameters:");
         passwordOptions.setFont(new Font("Serif",Font.PLAIN, 18));
--- SturdyPancakes/src/Register.java
+++ SturdyPancakes/src/Register.java
@@ -15,7 +15,7 @@ import javax.swing.*;
 public class Register extends JPanel{
     private JLabel enterUser,enterPass,registerStatus;
     private JTextField username,password;
-    private JButton register;
+    private JButton register,backButton;
     public Register(){
         super();
         setVisible(true);
@@ -23,42 +23,55 @@ public class Register extends JPanel{
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         
-        c.gridwidth = 1;
+        
+        
+        
+        c.gridwidth = 0;
         c.gridheight = 1;
-        c.weightx = 0;
-        c.weighty = 0;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1;
+        c.weighty = 1;
         c.ipady = 20;
         
+        backButton = new JButton("< Back");
+        backButton.setOpaque(false);
+        backButton.setContentAreaFilled(false);
+        c.anchor = GridBagConstraints.NORTHWEST;
+        add(backButton,c);
+        c.weightx = 0;
+        c.weighty = 0;
+        
         enterUser = new JLabel("Enter Username Below", SwingConstants.CENTER);
+        c.anchor = GridBagConstraints.CENTER;
         enterUser.setFont(new Font("Serif",Font.PLAIN, 18));
         c.gridx = 1;  
-        c.gridy = 5;
+        c.gridy = 1;
         c.insets = new Insets(350,0,0,0); 
         add(enterUser, c); 
         
         username = new JTextField();
         username.setFont(new Font("Serif",Font.PLAIN, 18));
         c.gridx = 1;   
-        c.gridy = 6;      
+        c.gridy = 2;      
         c.insets = new Insets(0,0,0,0); 
+        c.ipadx=160;
         add(username,c);
         
         enterPass = new JLabel("Enter Password Below", SwingConstants.CENTER);
         enterPass.setFont(new Font("Serif",Font.PLAIN, 18));
         c.gridx = 1;  
-        c.gridy = 7;  
+        c.gridy = 3;  
         add(enterPass,c);
         
         password = new JTextField();
         password.setFont(new Font("Serif",Font.PLAIN, 18));
         c.gridx = 1;  
-        c.gridy = 8;      
+        c.gridy = 4;      
         add(password, c);
         
         register = new JButton("Register");
+        c.ipadx = 0;
         c.gridx = 1;
-        c.gridy = 9;      
+        c.gridy = 5;      
         c.insets = new Insets(10,0,0,0); 
         register.setOpaque(false);
         register.setContentAreaFilled(false);
@@ -66,14 +79,18 @@ public class Register extends JPanel{
         
         registerStatus = new JLabel("",SwingConstants.CENTER);
         c.gridx = 1;
-        c.gridy = 10;      
+        c.gridy = 6;      
         c.insets = new Insets(0,0,0,0); 
         add(registerStatus,c);
+        
     }
     public JButton getRegister(){
         return register;
     }
     public JLabel getRegisterStatus(){
         return registerStatus;
+    }
+    public JButton getBack(){
+        return backButton;
     }
 }
