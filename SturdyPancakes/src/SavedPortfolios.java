5a21fbb061b6b854ab0d2ddb8db23305ee7da06d
Frank <Macks@66.71.78.148>
2016-12-12 14:08:44 -0500

    use your first name and last name as username, and password:password ex: username:Frank Liang password:password

--- SturdyPancakes/src/SavedPortfolios.java
+++ SturdyPancakes/src/SavedPortfolios.java
@@ -14,33 +14,56 @@ import java.util.ArrayList;
 public class SavedPortfolios {
     
     private ArrayList<String> portfolio;
+    private String label, username,password;
     
     public void savePortfolio(ArrayList<String> portfolio) {
         this.portfolio = new ArrayList<>();
         this.portfolio = portfolio;
+        label = "";
+        username = "";
+        password = "";
     }
     
     public void separateLines() {
         for (int i = 0; i < portfolio.size(); i++) {
             String temp = portfolio.get(i);
             int space = temp.indexOf("#");
-            String label = temp.substring(0,space);
+            setLabel(temp.substring(0,space));
             temp = temp.substring(space + 1);
             
             System.out.println("Label: " + label);
             
             space = temp.indexOf("#");
-            String username = temp.substring(0, space);
+            setUsername(temp.substring(0, space));
             temp = temp.substring(space + 1);
             
             System.out.println("Username: " + username);
             
-            String password = temp;
+            setPassword(temp);
             System.out.println("Password: " + password);
         }
+        
     }
     
     public ArrayList<String> getSavedPortfolio() {
         return portfolio;
+    }
+    public String getLabel(){
+        return label;
+    }
+    public void setLabel(String label){
+        this.label = label;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
     }
 }
